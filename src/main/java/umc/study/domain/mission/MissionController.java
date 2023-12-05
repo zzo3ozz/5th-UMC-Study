fa240610540623c3ef1c2b0e8776db6030216f4c
zzo3ozz <wityoung.h@gmail.com>
2023-12-06 05:29:24 +0900

    feat: 미션 추가 api 시그니처 생성

--- src/main/java/umc/study/domain/mission/MissionController.java
+++ src/main/java/umc/study/domain/mission/MissionController.java
@@ -3,10 +3,19 @@ package umc.study.domain.mission;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import umc.study.config.ResponseDto;
+import umc.study.domain.mission.model.MissionRequest;
+import umc.study.domain.validation.annotation.ExistStores;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/missions")
 @Validated
 public class MissionController {
+    private final MissionService missionService;
+
+    @PostMapping("/{storeID}")
+    public ResponseDto<Boolean> addMission(@PathVariable @ExistStores long storeID, @RequestBody MissionRequest missionRequest) {
+        return null;
+    }
 }
--- src/main/java/umc/study/domain/mission/MissionConverter.java
+++ src/main/java/umc/study/domain/mission/MissionConverter.java
@@ -1,4 +1,11 @@
 package umc.study.domain.mission;
 
+import umc.study.domain.mission.entity.Mission;
+import umc.study.domain.mission.model.MissionRequest;
+import umc.study.domain.store.entity.Store;
+
 public class MissionConverter {
+    public static Mission toMission(Store store, MissionRequest missionRequest) {
+        return null;
+    }
 }
